7848d417c200f47d3333d991ab7057fd90f4382b
新井昇平 <shohei.arai.52@gmail.com>
2015-11-03 19:33:47 +0900

    Add main flow

--- www/js/app.js
+++ www/js/app.js
@@ -3,17 +3,86 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic', 'starter.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
+    if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
-    if(window.StatusBar) {
+    if (window.StatusBar) {
       StatusBar.styleDefault();
     }
   });
 })
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider
+    .state('index', {
+      url: '/',
+      templateUrl: 'templates/menu.html',
+      // controller: 'BoardCtrl'
+    })
+    .state('board', {
+      url: '/board',
+      template: '<p>{{countDown}}</p>',
+      controller: function($scope, $interval, $state) {
+        $scope.countDown = 3;
+        var countId = $interval(function() {
+          if ($scope.countDown <= 1) {
+            $interval.cancel(countId);
+            $state.go('showing');
+          }
+          $scope.countDown--;
+        }, 800);
+      }
+    })
+    .state('showing', {
+      url: '/showing',
+      templateUrl: 'templates/board.html',
+      controller: 'BoardCtrl'
+    })
+    .state('end', {
+      url: '/end',
+      templateUrl: 'templates/menu.html',
+      controller: 'BoardCtrl'
+    });
+
+
+  // $stateProvider
+  //   .state('index', {
+  //     url: '/',
+  //     templateUrl: 'templates/tab-dash.html',
+  //   });
+    // .state('tab.chats', {
+    //   url: '/chats',
+    //   views: {
+    //     'tab-chats': {
+    //       templateUrl: 'templates/tab-chats.html',
+    //       controller: 'ChatsCtrl'
+    //     }
+    //   }
+    // })
+    // .state('tab.chat-detail', {
+    //   url: '/chats/:chatId',
+    //   views: {
+    //     'tab-chats': {
+    //       templateUrl: 'templates/chat-detail.html',
+    //       controller: 'ChatDetailCtrl'
+    //     }
+    //   }
+    // })
+    // .state('tab.account', {
+    //   url: '/account',
+    //   views: {
+    //     'tab-account': {
+    //       templateUrl: 'templates/tab-account.html',
+    //       controller: 'AccountCtrl'
+    //     }
+    //   }
+    // });
+});
---
+++ www/js/controllers.js
@@ -0,0 +1,75 @@
+angular.module('starter.controllers', [])
+
+.controller('BoardCtrl', function($scope, $stateParams, $interval) {
+  $scope.currentPoint = 0;
+  $scope.currentCount = 1;
+  $scope.isCorrect = false;
+
+  $scope.isShowBoard = true;
+  $scope.isShowEnd = false;
+
+  var countId = $interval(function() {
+    if ($scope.isShowBoard && !$scope.isCorrect) {
+      $scope.currentCount--;
+      if ($scope.currentCount <= 0) {
+        return;
+      }
+    }
+    $scope.isCorrect = false;
+    $scope.isShowBoard = !$scope.isShowBoard;
+
+    if (!$scope.isShowBoard) {
+      $scope.currentPoint++;
+    }
+  }, 800);
+
+  $scope.$watch('currentCount', function() {
+    if ($scope.currentCount <= 0) {
+      $interval.cancel(countId);
+      $scope.isShowEnd = true;
+    }
+  });
+})
+
+.directive('board', function() {
+  return {
+    restrict: 'A',
+    link: function(scope, elem) {
+      scope.indexQue = [4];
+      scope.currentIndex = [4];
+      var index = 0;
+      scope.$watch('isShowBoard', function() {
+        if (scope.isShowBoard) {
+          var maxNum = 9;
+          var num = Math.floor(Math.random() * maxNum);
+          scope.indexQue.push(num);
+          scope.currentIndex = scope.indexQue.pop();
+          angular.element(elem.find('button')[scope.currentIndex]).addClass('button-assertive lightCircle');
+        } else {
+          var buttons = angular.element(elem.find('button'));
+          buttons.text('');
+          angular.element(buttons[scope.currentIndex]).removeClass('button-assertive lightCircle');
+        }
+      });
+    }
+  };
+})
+
+.directive('circle', function() {
+  return {
+    restrict: 'C',
+    link: function(scope, elem) {
+      elem.on('touchstart', function() {
+        if (elem.hasClass('lightCircle')) {
+          scope.isCorrect = true;
+          elem.text('◯');
+        } else {
+          scope.currentCount--;
+          elem.text('×');
+        }
+      });
+    }
+  };
+})
+
+;
