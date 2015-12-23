c205a34495be92e2bf033dcdbd0125b1f372763a
新井昇平 <shohei.arai.52@gmail.com>
2015-12-23 17:43:32 +0900

    Change into no splash screen

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -128,24 +128,6 @@ angular.module('starter.controllers', ['ngStorage', 'ngCordova'])
         //     scope.$state.go('index');
         //   });
       });
-
-      scope.$watch('isEnd', function() {
-        if (!scope.isEnd) { return; }
-
-        var buttons = element.find('button');
-      //   console.log(angular.element(buttons[scope.correctIndex]).parent().parent().parent().parent().parent().parent()
-      // .find('#restart'));
-        // angular.element(buttons[scope.correctIndex]).addClass('selected');
-
-        // angular.element(buttons[scope.selectedIndex]).text('Menu')
-        //   .on('click', function() {
-        //     scope.$state.go('index');
-        //   });
-        // angular.element(buttons[scope.currentIndex]).text('Retry')
-        //   .on('click', function() {
-        //     scope.$state.reload();
-        //   });
-      });
     }
   };
 })
