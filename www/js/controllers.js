d83814bf23eb59f5accdab638ba86be6e2c82bf0
新井昇平 <shohei.arai.52@gmail.com>
2015-12-23 14:48:37 +0900

    Add title logo

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -130,10 +130,10 @@ angular.module('starter.controllers', ['ngStorage', 'ngCordova'])
       });
 
       scope.$watch('isEnd', function() {
-        if (!scope.isEnd) { return; }
+        // if (!scope.isEnd) { return; }
 
-        var buttons = element.find('button');
-        angular.element(buttons[scope.correctIndex]).text('Game Over');
+        // var buttons = element.find('button');
+        // angular.element(buttons[scope.correctIndex]).text('Game Over');
 
         // angular.element(buttons[scope.selectedIndex]).text('Menu')
         //   .on('click', function() {
