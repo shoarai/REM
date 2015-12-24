86773d0e4b1b4d125e93d70e118ec23a0f6ff2a9
新井昇平 <shohei.arai.52@gmail.com>
2015-12-24 19:31:46 +0900

    Add i18n

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,9 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', [
+  'ionic', 'pascalprecht.translate', 'starter.controllers'
+])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -16,6 +18,15 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       StatusBar.styleDefault();
     }
   });
+})
+
+.config(function($translateProvider) {
+  $translateProvider.useStaticFilesLoader({
+    prefix: 'js/lang/',
+    suffix: '.json'
+  });
+
+  $translateProvider.preferredLanguage('ja');
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
